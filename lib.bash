a7535e9d03fbc4aef0dcf0cee7c7a5e6fcf9597e
Hee-Suk Kim <hskim@dilluti0n.com>
2026-05-19 14:30:39 +0900

    sib-ask: fix -p does not take effect when -r turned on
    The problem was that the HEAD update by -p depended on user input, so
    I just branched at that point.
    
    Signed-off-by: Hee-Suk Kim <hskim@dilluti0n.com>

--- lib.bash
+++ lib.bash
@@ -1,3 +1,8 @@
 SCRIPT_DIR=$(cd "$(dirname "$0")" && pwd)
 export PATH="$SCRIPT_DIR:$SCRIPT_DIR/plm:$PATH"
 NAME=$(basename -- "$0")
+
+die() {
+    [ $# -eq 0 ] || echo fatal: "$@" >&2
+    exit 1
+}
